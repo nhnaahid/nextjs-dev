64c18322f2590e7945ca90a19de644b0bb876ede
nhnaahid <nhnahid.iu@gmail.com>
2024-10-19 13:06:36 +0600

    p added

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Next Dev</h1>
       <button className="text-xl">Click Me</button>
+      <p>Why no suggestion</p>
       <Heading></Heading>
     </div>
   );
