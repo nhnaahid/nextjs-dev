edd137d063b77bec8205a962b9992ded2b67e290
nhnaahid <nhnahid.iu@gmail.com>
2024-11-29 20:33:51 +0600

    added: detail of a post api

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,5 +1,6 @@
 
-const PostDetailsPage = () => {
+const PostDetailsPage = ({ params }) => {
+    console.log(params.id);
     return (
         <div>
             <h1>Post Details</h1>
--- src/services/postApi.js
+++ src/services/postApi.js
@@ -3,3 +3,9 @@ export const getPosts = async () => {
     const data = await res.json()
     return data;
 }
+
+export const getPostDetails = async (id) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    const data = await res.json();
+    return data;
+}
