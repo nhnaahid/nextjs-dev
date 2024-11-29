476d9f10ea0f7741194f6bf793a4768a00674490
nhnaahid <nhnahid.iu@gmail.com>
2024-11-29 10:41:49 +0600

    added: posts data fetching and show

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,8 +1,16 @@
+import { getPosts } from "@/services/postApi";
 
-const PostPage = () => {
+const PostPage = async () => {
+    const posts = await getPosts();
+    console.log(posts);
     return (
-        <div>
-            <p>Post page</p>
+        <div className="grid grid-cols-4 gap-3">
+            {
+                posts.slice(0, 20).map(post => <div key={post?.id} className="border text-center p-2 rounded-lg bg-blue-50">
+                    <h1 className="font-bold">{post?.title}</h1>
+                    <p>{post.body}</p>
+                </div>)
+            }
         </div>
     );
 };
