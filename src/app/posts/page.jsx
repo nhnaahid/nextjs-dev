4890e1d008b84232ba7cea23e190e3e62f976ae1
nhnaahid <nhnahid.iu@gmail.com>
2024-11-29 20:25:00 +0600

    added: button in post

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,4 +1,5 @@
 import { getPosts } from "@/services/postApi";
+import Link from "next/link";
 
 const PostPage = async () => {
     const posts = await getPosts();
@@ -6,9 +7,10 @@ const PostPage = async () => {
     return (
         <div className="grid grid-cols-4 gap-3">
             {
-                posts.slice(0, 20).map(post => <div key={post?.id} className="border text-center p-2 rounded-lg bg-blue-50">
+                posts.slice(0, 20).map(post => <div key={post?.id} className="border text-center p-2 rounded-lg bg-blue-50 space-y-3">
                     <h1 className="font-bold">{post?.title}</h1>
-                    <p>{post.body}</p>
+                    <p className="pb-5">{post.body}</p>
+                    <button className="pb-3"><Link href={`/posts/${post.id}`} className="border rounded-lg p-2 bg-gray-700 hover:bg-gray-800 duration-300 text-white">View Details</Link></button>
                 </div>)
             }
         </div>
