e8aaa4109193216303bf6ae6c87757f0f8472d47
nhnaahid <nhnahid.iu@gmail.com>
2024-11-29 20:42:47 +0600

    showing details data on post details page

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,9 +1,13 @@
+import { getPostDetails } from "@/services/postApi";
 
-const PostDetailsPage = ({ params }) => {
-    console.log(params.id);
-    return (
-        <div>
-            <h1>Post Details</h1>
+const PostDetailsPage = async ({ params }) => {
+    // console.log(params.id);
+    const postDetails = await getPostDetails(params.id)
+    // console.log(postDetails);
+    return ( 
+        <div className="text-center w-[80%] mx-auto bg-sky-50 rounded-lg p-3">
+            <h1 className="font-bold">{postDetails.id} : {postDetails.title}</h1>
+            <p>{postDetails.body}</p>
         </div>
     );
 };
