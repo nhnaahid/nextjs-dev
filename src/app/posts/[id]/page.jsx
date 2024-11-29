ff112de1ffa8272f0c3f82dd00a75773b238a841
nhnaahid <nhnahid.iu@gmail.com>
2024-11-29 20:44:50 +0600

    update: post details

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,11 +1,9 @@
 import { getPostDetails } from "@/services/postApi";
 
 const PostDetailsPage = async ({ params }) => {
-    // console.log(params.id);
     const postDetails = await getPostDetails(params.id)
-    // console.log(postDetails);
-    return ( 
-        <div className="text-center w-[80%] mx-auto bg-sky-50 rounded-lg p-3">
+    return (
+        <div className="text-center w-[75%] mx-auto bg-sky-50 rounded-lg p-3 space-y-2">
             <h1 className="font-bold">{postDetails.id} : {postDetails.title}</h1>
             <p>{postDetails.body}</p>
         </div>
